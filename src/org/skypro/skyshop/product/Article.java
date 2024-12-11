074de2b1235d7b8b5a436e4b34a920ab1ab579b6
sbakhareva <snezhana.bakhareva@yandex.ru>
2024-12-11 11:58:46 +0300

    ничего не работает

--- src/org/skypro/skyshop/product/Article.java
+++ src/org/skypro/skyshop/product/Article.java
@@ -3,7 +3,7 @@ package org.skypro.skyshop.product;
 import org.skypro.skyshop.SearchEngine;
 import org.skypro.skyshop.Searchable;
 
-public class Article extends SearchEngine implements Searchable {
+public class Article implements Searchable {
     private final String title;
     private final String text;
 
